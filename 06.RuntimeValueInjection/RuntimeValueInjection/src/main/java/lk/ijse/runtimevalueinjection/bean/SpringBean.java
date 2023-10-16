c37296cedc6ff0fbbbe5f0ae58a382a5df1fe9a4
BinaraDulsara <binaradulsara021@gmail.com>
2023-10-16 08:09:06 +0530

    value annotation is introduced runtime value injection module

--- 06.RuntimeValueInjection/RuntimeValueInjection/src/main/java/lk/ijse/runtimevalueinjection/bean/SpringBean.java
+++ 06.RuntimeValueInjection/RuntimeValueInjection/src/main/java/lk/ijse/runtimevalueinjection/bean/SpringBean.java
@@ -1,10 +1,12 @@
 package lk.ijse.runtimevalueinjection.bean;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean {
-    public SpringBean() {
+    public SpringBean(@Value("C001") String id) {
         System.out.println("SpringBean instantiated");
+        System.out.println(id);
     }
 }
