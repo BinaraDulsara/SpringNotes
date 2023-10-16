c7d715471e34a25e3d21931bf17ec626893ea4d4
BinaraDulsara <binaradulsara021@gmail.com>
2023-10-16 07:02:53 +0530

    content update

--- 05.Configuration/Configuration/src/main/java/lk/ijse/configuration/config/AppConfig.java
+++ 05.Configuration/Configuration/src/main/java/lk/ijse/configuration/config/AppConfig.java
@@ -4,10 +4,17 @@ package lk.ijse.configuration.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.configuration.bean")
-@Import({AppConfig1.class, AppConfig2.class})
+
+// There are two ways to configure,
+//                                1.annotation
+//                                2.xml
+//@Import({AppConfig1.class, AppConfig2.class}) // we can import configuration into a single class
+//@ImportResource("file:absolute-path-of-abcd.xml") // we can use this annotation to import xml Configurations to the context
+
 public class AppConfig {
 
  }
