56a34104d853da33e6e5f54086ed64e85133ed75
BinaraDulsara <binaradulsara021@gmail.com>
2023-10-16 22:24:26 +0530

    How to Access Environment variables in JAVA

--- 07.SpringEnviornment/src/main/java/lk/ijse/springenvironment/AppInitializer.java
+++ 07.SpringEnviornment/src/main/java/lk/ijse/springenvironment/AppInitializer.java
@@ -3,13 +3,26 @@ package lk.ijse.springenvironment;
 import lk.ijse.springenvironment.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class Appinitializer {
+import java.util.Map;
+
+public class AppInitializer {
     public static void main(String[] args) {
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.register(AppConfig.class);
         context.refresh();
         context.registerShutdownHook();
+
+
+
+        // System.env ---> Access to System Environment with JAVA
+
+        Map<String, String> getenv = System.getenv();
+        for (String key : getenv.keySet()) {
+            System.out.println (key+" : "+getenv.get(key));
+
+        }
+        
 
     }
 }
