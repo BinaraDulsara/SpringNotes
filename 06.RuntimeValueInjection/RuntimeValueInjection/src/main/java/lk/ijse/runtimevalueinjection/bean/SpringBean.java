12a6df5c0a107351072c5f1b8a83a159ec60b089
BinaraDulsara <binaradulsara021@gmail.com>
2023-10-16 08:44:40 +0530

    use instance properties

--- 06.RuntimeValueInjection/RuntimeValueInjection/src/main/java/lk/ijse/runtimevalueinjection/bean/SpringBean.java
+++ 06.RuntimeValueInjection/RuntimeValueInjection/src/main/java/lk/ijse/runtimevalueinjection/bean/SpringBean.java
@@ -1,12 +1,32 @@
 package lk.ijse.runtimevalueinjection.bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean {
-    public SpringBean(@Value("C001") String id) {
+
+ /*   @Autowired(required = false)
+    public SpringBean(@Value("C001") String id,@Value("20") int age ,@Value("true") boolean b ) {
         System.out.println("SpringBean instantiated");
         System.out.println(id);
+        System.out.println(age);
+        System.out.println(b);
     }
+
+    @Autowired(required = false)
+    public SpringBean(@Value("1,2,3 ") int [] myNames ,@Value("A") char a) {
+        System.out.println("SpringBean instantiated");
+        for (int myName : myNames) {
+            System.out.println(myName);
+        }
+        System.out.println(a);
+
+        }
+*/
+
+
+
+
 }
---
+++ 06.RuntimeValueInjection/RuntimeValueInjection/src/main/java/lk/ijse/runtimevalueinjection/bean/SpringBeanTwo.java
@@ -0,0 +1,40 @@
+package lk.ijse.runtimevalueinjection.bean;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBeanTwo implements InitializingBean {
+    @Value("C001")
+    private String id;
+
+    @Value("binara")
+    private String name;
+
+    @Value("wadduwa")
+    private String address;
+
+    @Value("20000")
+    private double salary;
+
+    public SpringBeanTwo() {
+        System.out.println("SpringBeanTwo instantiated");
+        System.out.println(id);
+        System.out.println(name);
+        System.out.println(address);
+        System.out.println(salary);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("-----------------------------------------");
+        System.out.println(id);
+        System.out.println(name);
+        System.out.println(address);
+        System.out.println(salary);
+
+    }
+}
+
+
